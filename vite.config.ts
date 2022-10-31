eeb5427a40080abb2ba7e1a8d1849370176f5d3a
aaronespasa <aaespasa@gmail.com>
2022-10-31 09:22:59 +0100

    ViteJS base files

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import analyze from 'rollup-plugin-analyzer'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      plugins: [
+        analyze({summaryOnly: true, limit: 10}),
+      ],
+    },
+  },
+  plugins: [react()]
+})
